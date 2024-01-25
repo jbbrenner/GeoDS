450202f11689ae1d4e15a940abfc51b52fd17991
Jean-Baptiste Brenner <jean-baptiste.brenner@lsce.ipsl.fr>
2024-01-25 15:48:39 +0100

    updates

--- src/Reading_variables.f90
+++ src/Reading_variables.f90
@@ -19,7 +19,7 @@ CONTAINS
     REAL, DIMENSION(256,156)  :: elev
     INTEGER,INTENT(OUT) :: n
 
-    CALL nc_read("/home/jbrenner/These/Cartes/S.nc","orog",elev)
+    CALL nc_read("/home/jbrenner/These/Cartes/S.nc","orog",eleva)
     
     n=SIZE(elev)
     PRINT*,"le tableau a une taille de",n
