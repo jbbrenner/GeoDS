045e077b27ffc2383715957bf40cdec8069fb9e5
Jean-Baptiste Brenner <jean-baptiste.brenner@lsce.ipsl.fr>
2024-01-31 08:22:45 +0100

    Creation of the configuration file containing path & variables required to read and store netCDF data

--- app/main.f90
+++ app/main.f90
@@ -1,34 +1,47 @@
 PROGRAM main
-  !----------------------------------------------------------!
-  !Import modules
-  !----------------------------------------------------------!
+  !________________________________________________________________________________________!
+  !Importing modules
+  !________________________________________________________________________________________!
+
   USE Parametrization
+  USE Reading_Variables
   USE Temperature
   USE ncio, ONLY: nc_read
-  !USE Reading_Variables
-  !----------------------------------------------------------!
-  !
-  !----------------------------------------------------------!
+  !________________________________________________________________________________________!
+
   IMPLICIT NONE
-  !----------------------------------------------------------!
-  !Declare variables
-  !----------------------------------------------------------!
+
+  !________________________________________________________________________________________!
+  !Declaring variables
+  !________________________________________________________________________________________!  
+
+  CHARACTER (LEN=100) :: LR_temperature_dataset_file
+  CHARACTER (LEN=20) :: LR_surface_temperature_id
+  REAL, DIMENSION(64,32,1200) :: LR_surface_temperature_data
+  CHARACTER (LEN=100) :: HR_elevation_file
+  CHARACTER (LEN=20) :: HR_surface_elevation_id
+  REAL, DIMENSION(71,61) :: HR_surface_elevation_data
+
+
+  
   REAL :: T
   CHARACTER(LEN=10) :: interpol
 
-  REAL, DIMENSION(256,156)  :: eleva
-  INTEGER :: n
-  
-
-  !----------------------------------------------------------!
-  ! Call external functions and subroutines
-  !----------------------------------------------------------!
+  !________________________________________________________________________________________!
+  ! Calling external functions and subroutines
+  !________________________________________________________________________________________!
   CALL Test(3.0,T)
-  CALL nc_read("/home/jbrenner/These/Cartes/S.nc","orog",eleva)
+  
+  CALL inputs_temperature(LR_temperature_dataset_file, LR_surface_temperature_id, LR_surface_temperature_data)
 
-  !CALL Lecture(n,elev)
+    PRINT *,LR_surface_temperature_data(1,1,1)
+
+  CALL inputs_topography(HR_elevation_file, HR_surface_elevation_id, HR_surface_elevation_data)
+
+    PRINT *,HR_surface_elevation_data(1,1)
 
 
+  
   !----------------------------------------------------------!
   ! Apply interpolation using CDO
   ! Bash script located in src
@@ -44,5 +57,7 @@ PROGRAM main
 
   
   WRITE(*,*)"end of the program"
+
+  !________________________________________________________________________________________!
 
 END PROGRAM main
--- src/Reading_variables.f90
+++ src/Reading_variables.f90
@@ -1,33 +1,97 @@
 MODULE Reading_Variables
   
-  !-----------------------------------------------------------------------!
+  !________________________________________________________________________________________!
+
+  USE Parametrization
   USE ncio, ONLY: nc_read
-  !-----------------------------------------------------------------------! 
+
+  !________________________________________________________________________________________! 
+
   IMPLICIT NONE
 
-  !-----------------------------------------------------------------------!
- 
-  !-----------------------------------------------------------------------!
+  !________________________________________________________________________________________!
+  !________________________________________________________________________________________!
 
 CONTAINS
 
-
-  SUBROUTINE Lecture(n,elev)
+  !Subroutines required to read netCDF datasets and store related data in arrays using the ncio library. All the subroutines
+  !open the same configuration file containing paths and other parametrization variables (namelist), but each one is associated
+  !with a different category of data (i.e : data related to temperature, precipitation, topography...)
+  
+  SUBROUTINE inputs_temperature(LR_temperature_dataset_file, LR_surface_temperature_id,LR_surface_temperature_data)
 
     IMPLICIT NONE
 
-    REAL, DIMENSION(256,156)  :: elev
-    INTEGER,INTENT(OUT) :: n
+    CHARACTER (LEN=100), INTENT(INOUT) :: LR_temperature_dataset_file
+    CHARACTER (LEN=20), INTENT(INOUT) :: LR_surface_temperature_id
+    REAL, DIMENSION(64,32,1200), INTENT(INOUT) :: LR_surface_temperature_data
+    INTEGER :: ios,fu
+    LOGICAL :: input_checking
 
-    CALL nc_read("/home/jbrenner/These/Cartes/S.nc","orog",elev)
-    
-    n=SIZE(elev)
-    PRINT*,"le tableau a une taille de",n
-
-
-  END SUBROUTINE Lecture
+    NAMELIST/Temperature/LR_temperature_dataset_file, LR_surface_temperature_id
 
 
+    !Checking whether the configuration file exists or not
+    INQUIRE (file=Configuration_file, EXIST=input_checking)
+
+        IF (input_checking .eqv. (.FALSE.)) THEN
+            WRITE (*, *)"Error: input file ", Configuration_file, " does not exist"
+         END IF
+
+    !Opening configuration file to access netCDF files paths (temperature data)
+    OPEN (ACTION='READ', FILE=Configuration_file, IOSTAT=ios, NEWUNIT=fu)
+        IF (ios /= 0) THEN
+            WRITE (*, *)"Error:",Configuration_file,"could not be opened"
+         END IF
+    READ (NML=Temperature, IOSTAT=ios, UNIT=fu)
+
+    !Storing temperature data from LR netCDF file in array
+    CALL nc_read(LR_temperature_dataset_file, LR_surface_temperature_id, LR_surface_temperature_data)
+
+    !Closing configuration file
+    CLOSE(fu)
+
+  END SUBROUTINE inputs_temperature
+
+  !________________________________________________________________________________________!
+  !________________________________________________________________________________________!
+
+  SUBROUTINE inputs_topography(HR_elevation_file, HR_surface_elevation_id,HR_surface_elevation_data)
+
+    IMPLICIT NONE
+
+    CHARACTER (LEN=100), INTENT(INOUT) :: HR_elevation_file
+    CHARACTER (LEN=20), INTENT(INOUT) :: HR_surface_elevation_id
+    REAL, DIMENSION(71,61), INTENT(INOUT) :: HR_surface_elevation_data
+    INTEGER :: ios,fu
+    LOGICAL :: input_checking
+
+    NAMELIST/Topography/HR_elevation_file, HR_surface_elevation_id
 
 
+    !Checking whether the configuration file exists or not
+    INQUIRE (file=Configuration_file, EXIST=input_checking)
+
+        IF (input_checking .eqv. (.FALSE.)) THEN
+            WRITE (*, *)"Error: input file ", Configuration_file, " does not exist"
+         END IF
+
+    !Opening configuration file to access netCDF files paths (temperature data)
+    OPEN (ACTION='READ', FILE=Configuration_file, IOSTAT=ios, NEWUNIT=fu)
+        IF (ios /= 0) THEN
+            WRITE (*, *)"Error:",Configuration_file,"could not be opened"
+         END IF
+    READ (NML=Topography, IOSTAT=ios, UNIT=fu)
+
+    !Storing temperature data from LR netCDF file in array
+    CALL nc_read(HR_elevation_file, HR_surface_elevation_id, HR_surface_elevation_data)
+
+    !Closing configuration file
+    CLOSE(fu)
+
+  END SUBROUTINE inputs_topography
+
+  !________________________________________________________________________________________!
+  !________________________________________________________________________________________!
+  
 END MODULE Reading_Variables
