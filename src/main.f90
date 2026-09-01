17b53f0bb38845508a53afef879e0ea4703cc19b
Jean-Baptiste Brenner <jbrenner@obelix4.lsce.ipsl.fr>
2026-09-01 13:17:17 +0200

    test

--- src/main.f90
+++ src/main.f90
@@ -20,8 +20,8 @@ PROGRAM main
 
   INTEGER :: p
 
-  PRINT*, "____________________________________________"
-  PRINT*, "Reading Inputs"
+  !! PRINT*, "____________________________________________"
+  !! PRINT*, "Reading Inputs"
 
   CALL reading_temperature_inputs(lr_surface_temperature_data, config_namelist_blockname, t_extent, ios, fu)
 
@@ -32,9 +32,9 @@ PROGRAM main
   CALL reading_topography_inputs(lr_surface_elevation_data, hr_surface_elevation_data, lr_topographic_insolation_data, &
        hr_topographic_insolation_data, config_namelist_blockname, ios, fu)
 
-  PRINT*, "____________________________________________"
-  PRINT*, "Computing topography-based parameters"
-  PRINT*, ""
+  !! PRINT*, "____________________________________________"
+  !! PRINT*, "Computing topography-based parameters"
+  !! PRINT*, ""
   CALL filling_WL_patterns_arrays(WL_pattern_pointers_array,wdir_angle_boundaries)
  
   CALL computing_elevation_anomalies(lr_surface_elevation_data, hr_surface_elevation_data, elevation_anomalies_data)
@@ -43,25 +43,24 @@ PROGRAM main
        topographic_insolation_anomalies_data)
   
   CALL computing_WL_exposure_indexes(TEI_pointers_array, TEI_drying_effect_correction)
-  PRINT*, "TEST JB", TEI_drying_effect_correction(1)%tei_arr_ptr(100,100)
-  CALL writing_wdir_gridpoints_patterns()
-
+  !CALL writing_wdir_gridpoints_patterns()
+  
   CALL computing_wind_directions(sorted_wind_directions_data)
 
-  PRINT*, "____________________________________________"
-  PRINT*, "Downscaling temperature"
+  !!PRINT*, "____________________________________________"
+  !!PRINT*, "Downscaling temperature"
   
   CALL applying_lapse_rate_correction(lr_surface_temperature_data,elevation_anomalies_data, &
        hr_surface_temperature_data, hr_lr_surface_temperature_anomalies)
 
-  PRINT*, "____________________________________________"
-  PRINT*, "Downscaling precipitation"
+  !!PRINT*, "____________________________________________"
+  !!PRINT*, "Downscaling precipitation"
   
   CALL downscaling_precipitation(lr_precipitation_data, hr_precipitation_data, &
        hr_lr_precipitation_ratio, hr_lr_precipitation_anomalies)
 
-  PRINT*, "____________________________________________"
-  PRINT*, "Writing Outputs"
+  !!PRINT*, "____________________________________________"
+  !!PRINT*, "Writing Outputs"
    
   CALL initializing_downscaled_outputs_grid(ds_x_grid,ds_y_grid, tei_wdir_grid, ds_monthly_t_grid,&
        ds_annual_t_grid, config_namelist_blockname, ios, fu)
@@ -103,8 +102,8 @@ PROGRAM main
      DEALLOCATE(ds_annual_t_grid, ds_annual_temperature_data)
   ENDIF
   !________________________________________________________________________________________!
-  PRINT*, "____________________________________________"
-  WRITE(*,*)"Closing program"
+  !!PRINT*, "____________________________________________"
+  !!WRITE(*,*)"Closing program"
   !________________________________________________________________________________________!
 
 END PROGRAM main
